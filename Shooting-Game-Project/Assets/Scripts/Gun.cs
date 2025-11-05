14d616b9183eec1136123488daef406818ceda0c
64mik-dev <64mik.dev@gmail.com>
2025-11-05 18:33:18 +0900

    총 스크립트 조금 수정
    코드 리펙토링 및 총알 설정 변수 추가.

--- Shooting-Game-Project/Assets/Scripts/Gun.cs
+++ Shooting-Game-Project/Assets/Scripts/Gun.cs
@@ -3,28 +3,29 @@ using UnityEngine.InputSystem;
 
 public class Gun : MonoBehaviour
 {
+    [Header("발사 관련 설정")]
     public GameObject bulletPrefab; //총알 프리팹
     public Transform firePoint; //총구 위치
-    public float bulletSpeed = 20f; //생성할 총알 속도
-    public float fireRate = 0.5f;   //총 발사 후 지연 시간
-    public int maxAmmo = 10;    //최대 장탄 수
     public ParticleSystem muzzleFlash;  //총 쏘면 번쩍이는 거, 나중에 추가 요함
     public AudioClip shootSound;    //총 소리, 총 소리도 아직 없음
 
+    [Header("총알 설정")]
+    public int maxBullet = 10;    //최대 장탄 수
+    public float fireRate = 0.5f;   //총 발사 후 지연 시간
+    public float bulletSpeed = 20f; //생성할 총알 속도
+    public float damage = 1f;      
+ 
     private int bulletsLeft;    //남은 총알 수
     private float nextFireTime; //다음 총 발사 가능 시간
 
     private void Awake()
     {
-        Debug.Log("start");
-        bulletsLeft = maxAmmo;
-
+        bulletsLeft = maxBullet;
     }
     public void OnShoot()
     {
         if (Time.time >= nextFireTime)
         {
-            Debug.Log("action called");
             Shoot();
             nextFireTime = Time.time + fireRate;
         }
@@ -33,9 +34,7 @@ public class Gun : MonoBehaviour
 
     public void OnReload()
     {
-            Debug.Log("action called");
             Reload();
-     
     }
 
     private void Shoot()
@@ -60,7 +59,6 @@ public class Gun : MonoBehaviour
                 if (muzzleFlash != null)
                     muzzleFlash.Play();
 
-
                 bulletsLeft--;
             }
         }
@@ -69,7 +67,7 @@ public class Gun : MonoBehaviour
 
     public void Reload()
     {
-        bulletsLeft = maxAmmo;
+        bulletsLeft = maxBullet;
         Debug.Log("재장전 완료!");
     }
 }
